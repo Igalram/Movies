87b4e1ed868a1d953f7c6f62f4baf566ae06db16
iiram <ram8555@gmail.com>
2018-07-10 15:18:47 +0300

    finished second get http

--- convert.js
+++ convert.js
@@ -5,14 +5,12 @@ app.factory('convertService', function() {
 
     wordRate = function (x) {
         var wordRateString = "";
-        console.log(x);
         if (x<=10 && x>=8) {wordRateString = "Awesome"}
         else if (x>=7 && x<8) {wordRateString = "Good"}
         else if (x>=6 && x<7) {wordRateString = "Mediocre"}
         else if (x>=5 && x<6) {wordRateString = "Sucks"}
         else if (x>=0 && x<5) {wordRateString = "Terrible"}
         else {console.log("Error in conditions")}
-        console.log(wordRateString);
         return wordRateString;
 
        
